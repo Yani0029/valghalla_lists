652ab27cd8c765f0c0b353781673c9a6cbce0047
Yani0029 <yani@yanis-air.home>
2014-04-29 22:51:55 +0200

    Fix mulit polling stations in list

--- liste_valghalla_kvittering/liste_valghalla_kvittering.tpl.php
+++ liste_valghalla_kvittering/liste_valghalla_kvittering.tpl.php
@@ -1,16 +1,14 @@
 <div class="valgsteder-kvittering-list">
 <?php if($lists): ?>
   <?php foreach($lists as $nid => $constituency): ?>
-  <h3><?php print "Valgkreds: <a href='/node/$nid'>".$constituency['title']."</a>"; ?></h3>
+  <h3><?php print "Valgkreds: <a href='/node/$nid'>".$constituency['c_title']."</a>"; ?></h3>
   <ul>
-    <?php foreach($constituency as $key => $number): ?>
-      <?php if(is_numeric($key)): ?>
+    <?php foreach($constituency['nodes'] as $key => $number): ?>
       <li>
         <?php print "<a href='/valghalla_lists/kvittering/$key'>".$number['title']."</a>"; ?>
       </li>
-      <?php endif;?>
-  </ul>
     <?php endforeach; ?>
+  </ul>
   <?php endforeach; ?>
   <?php elseif (!$user_restrict_stations): ?>
   <div></div>
